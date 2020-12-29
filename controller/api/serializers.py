18d9080341a65a79dd0c8bfd9b1faa851576febc
June <zhoujune0506@gmail.com>
2020-12-29 16:54:54 +0800

    first commit

---
+++ controller/api/serializers.py
@@ -0,0 +1,10 @@
+# Transfer the models to JSON
+from rest_framework import serializers
+from .models import Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause',
+                  'votes_to_skip', 'created_at')
