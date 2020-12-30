8d03b96a399ae03458619843089d2c417d835a7c
June <zhoujune0506@gmail.com>
2020-12-30 19:08:57 +0800

    2020-12-30

--- controller/api/serializers.py
+++ controller/api/serializers.py
@@ -1,4 +1,3 @@
-# Transfer the models to JSON
 from rest_framework import serializers
 from .models import Room
 
@@ -8,3 +7,9 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause',
                   'votes_to_skip', 'created_at')
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
