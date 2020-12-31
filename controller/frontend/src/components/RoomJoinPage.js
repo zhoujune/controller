45d01c295567cd411a93f9e6fb3b283c0d10cddf
June <zhoujune0506@gmail.com>
2020-12-31 20:43:25 +0800

    12-31 update Happy new year

---
+++ controller/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class HomePage extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(<p>Room join page asdasdasd</p>)
+    }
+} 
